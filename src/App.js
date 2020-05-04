ab39f628cea4018d8955ed4680e0533e2f370cdc
Maksim <maksim-nemtsev@users.noreply.github.com>
2020-05-04 11:32:13 +0300

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <header className="App-header">
+      <p>Hello</p>
+    </header>
+  );
+}
+
+export default App;
